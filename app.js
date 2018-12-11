75ff2dce59d801fb05c38e1d81a03e2a689010bf
Brayan <brayan.giraldo@mowin-tech.com>
2018-12-11 14:41:46 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
 // Inicializar variables
 var app = express();
+
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la base de datos
 
@@ -16,14 +30,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err , res) 
 });
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(404).json({
-        ok: true,
-        mensaje: 'Peticion realizada con exito'
-    });
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
